d9d271d5f0401309c9a537bf850f2fa7a7c0c27f
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-23 18:48:43 +0530

    login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Container className="login-container p-5">
+          <Row className="justify-content-md-center mt-2">
+            <h3>Sign In</h3>
+          </Row>
+          <Row className="d-flex flex-column mt-5 justify-content-md-center">
+            <form action="" method="" className="d-flex flex-column">
+              <input type="email" name="email" id="login-email" className="p-2" value="Email" />
+              <input type="password" name="password" id="login-password" className="mt-3 p-2" value="Password" />
+              <Button variant="primary" className="mt-3">LOGIN</Button>
+            </form>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
